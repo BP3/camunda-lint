0d4b63c401fc6a659960c90414d5d766a7b98e10
Filipe Pereira <fpereira@bp-3.com>
2025-12-10 13:54:54 +0000

    clean up redundant colors from the logger script

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -6,19 +6,12 @@
 # We use octal escapes \033 for maximum portability with printf
 RESET='\033[0m'
 BOLD='\033[1m'
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
 GRAY='\033[90m'
 
 RED_BRIGHT='\033[91m'     # chalk.redBright
 GREEN_BRIGHT='\033[92m'   # chalk.greenBright
 YELLOW_BRIGHT='\033[93m'  # chalk.yellowBright
 BLUE_BRIGHT='\033[94m'    # chalk.blueBright
-MAGENTA_BRIGHT='\033[95m' # chalk.magentaBright
-CYAN_BRIGHT='\033[96m'    # chalk.cyanBright
-WHITE_BRIGHT='\033[97m'   # chalk.whiteBright
 
 # ==========================================
 # Logging Functions
